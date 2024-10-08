d1074d66918f68b389ee3d69a25592aa90317bfc
ikeraguero <ikerpires407@gmail.com>
2024-10-08 17:21:33 -0300

    Add Polymorphism Section Exercises

---
+++ 02-polymorphism/02-polymorphism/Ford.java
@@ -0,0 +1,19 @@
+public class Ford extends Car{
+    public Ford(int cylinders, String name) {
+        super(cylinders, name);
+    }
+    @Override
+    public String startEngine() {
+        return (getClass().getSimpleName() + " is starting");
+    }
+
+    @Override
+    public String accelerate() {
+        return (getClass().getSimpleName() + " is accelerating");
+    }
+
+    @Override
+    public String brake() {
+        return (getClass().getSimpleName() + " is braking");
+    }
+}
---
+++ 02-polymorphism/02-polymorphism/Holden.java
@@ -0,0 +1,20 @@
+public class Holden extends Car{
+    public Holden(int cylinders, String name) {
+        super(cylinders, name);
+    }
+
+    @Override
+    public String startEngine() {
+        return (getClass().getSimpleName() + " is starting");
+    }
+
+    @Override
+    public String accelerate() {
+        return (getClass().getSimpleName() + " is accelerating");
+    }
+
+    @Override
+    public String brake() {
+        return (getClass().getSimpleName() + " is braking");
+    }
+}
